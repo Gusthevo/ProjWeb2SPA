bf9a9438f404c65c26b7c0da857b1688024a4aa4
Gustavo Campelo <gustavocraft321@gmail.com>
2023-07-12 22:41:29 -0300

    commit login backend

---
+++ src/main/java/com/example/loginpage/model/User.java
@@ -0,0 +1,57 @@
+package com.example.loginpage.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String email;
+
+    private String usuario;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void getEmail(String email) {
+        this.email = email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
